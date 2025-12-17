4415fe5f4a2ff940c97ffc2c2d7dbd611bc69f09
hhhk412 <pqqqqw@mail.ru>
2025-12-17 19:31:47 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,25 @@
 
 
 if __name__ == "__main__":
-    pass # Ваш код здесь
+def calc(s, lvl=0):
+    if len(s) == 1:          # одно число
+        print("  "*lvl + s)
+        return int(s)
+
+    if s[0] == "(":
+        return calc(s[1:-1], lvl+1)
+
+    b = 0
+    for i in range(len(s)):
+        if s[i] == "(":
+            b += 1
+        elif s[i] == ")":
+            b -= 1
+        elif b == 0 and s[i] in "+-*":
+            a = calc(s[:i], lvl+1)
+            c = calc(s[i+1:], lvl+1)
+            r = a + c if s[i] == "+" else a - c if s[i] == "-" else a * c
+            print("  "*lvl + f"{a} {s[i]} {c} = {r}")
+            return r
+
+print("Результат:", calc("(2+(3*(4-1)))"))
