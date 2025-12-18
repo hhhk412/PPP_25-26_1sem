39e936ff4c7ce01854cbbdfb815b5c4f259fb4c4
hhhk412 <pqqqqw@mail.ru>
2025-12-18 20:50:51 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -2,12 +2,14 @@
 
 if __name__ == "__main__":
 def calc(s, lvl=0):
-    if len(s) == 1:         
-        print("  "*lvl + s)
+    s = s.replace(" ", "")
+
+    if s.isdigit():
+        print("  " * lvl + s)
         return int(s)
 
-    if s[0] == "(":
-        return calc(s[1:-1], lvl+1)
+    if s[0] == "(" and s[-1] == ")":
+        return calc(s[1:-1], lvl + 1)
 
     b = 0
     for i in range(len(s)):
@@ -15,11 +17,28 @@ def calc(s, lvl=0):
             b += 1
         elif s[i] == ")":
             b -= 1
-        elif b == 0 and s[i] in "+-*":
-            a = calc(s[:i], lvl+1)
-            c = calc(s[i+1:], lvl+1)
-            r = a + c if s[i] == "+" else a - c if s[i] == "-" else a * c
-            print("  "*lvl + f"{a} {s[i]} {c} = {r}")
+        elif b == 0 and s[i] in "+-":
+            a = calc(s[:i], lvl + 1)
+            c = calc(s[i + 1:], lvl + 1)
+            if s[i] == "+":
+                r = a + c
+            else:
+                r = a - c
+            print("  " * lvl + f"{a} {s[i]} {c} = {r}")
             return r
 
-print("Результат:", calc("(2+(3*(4-1)))"))
+    b = 0
+    for i in range(len(s)):
+        if s[i] == "(":
+            b += 1
+        elif s[i] == ")":
+            b -= 1
+        elif b == 0 and s[i] == "*":
+            a = calc(s[:i], lvl + 1)
+            c = calc(s[i + 1:], lvl + 1)
+            r = a * c
+            print("  " * lvl + f"{a} * {c} = {r}")
+            return r
+
+
+print("Результат:", calc("(2+(3*(4-1)))+1+1+1"))
